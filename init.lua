1f3d78e1676af693fad0bacb0c75676c18f836b9
theBestPatate <jean-pierre.gninahophin@etu.univ-amu.fr>
2025-04-26 20:57:10 +0000

    typst support and a beautiful markdown viewer inside neovim

--- init.lua
+++ init.lua
@@ -973,6 +973,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'custom.plugins.editing',
   require 'custom.plugins.dev',
+  require 'custom.plugins.note_taking',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
