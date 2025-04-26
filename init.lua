19750e69c0734dd97992575f92c4784872864172
theBestPatate <jean-pierre.gninahophin@etu.univ-amu.fr>
2025-04-26 20:48:46 +0000

    dev plugins: Vim fugitive from Tpope

--- init.lua
+++ init.lua
@@ -972,6 +972,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'custom.plugins.editing',
+  require 'custom.plugins.dev',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
---
+++ lua/custom/plugins/dev.lua
@@ -0,0 +1,4 @@
+return {
+  -- NOTE: All git commands directly from neovim
+  { 'tpope/vim-fugitive', enabled = true },
+}
