38dc5e077a24975f5e9624f6752c3b58cb0ef47f
theBestPatate <jean-pierre.gninahophin@etu.univ-amu.fr>
2025-04-26 21:01:06 +0000

    markdown toc

--- lua/custom/plugins/note_taking.lua
+++ lua/custom/plugins/note_taking.lua
@@ -12,4 +12,26 @@ return {
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     opts = {},
   },
+
+  { -- show tree of symbols in the current file
+    'hedyhli/outline.nvim',
+    cmd = 'Outline',
+    keys = {
+      { '<leader>lo', ':Outline<cr>', desc = 'symbols outline' },
+    },
+    opts = {
+      providers = {
+        priority = { 'markdown', 'lsp' },
+        -- Configuration for each provider (3rd party providers are supported)
+        lsp = {
+          -- Lsp client names to ignore
+          blacklist_clients = {},
+        },
+        markdown = {
+          -- List of supported ft's to use the markdown provider
+          filetypes = { 'markdown', 'quarto' },
+        },
+      },
+    },
+  },
 }
