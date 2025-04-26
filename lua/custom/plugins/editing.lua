78db88c31e1149ef9ac4b728dcf4c122a405f0ab
theBestPatate <jean-pierre.gninahophin@etu.univ-amu.fr>
2025-04-26 20:42:51 +0000

    initial editing plugins

---
+++ lua/custom/plugins/editing.lua
@@ -0,0 +1,60 @@
+return {
+
+  -- disables hungry features for files larget than 2MB
+  { 'LunarVim/bigfile.nvim' },
+
+  -- add/delete/change can be done with the keymaps
+  -- ys{motion}{char}, ds{char}, and cs{target}{replacement}
+  {
+    'kylechui/nvim-surround',
+    event = 'VeryLazy',
+    opts = {},
+  },
+
+  { -- commenting with e.g. `gcc` or `gcip`
+    -- respects TS, so it works in quarto documents 'numToStr/Comment.nvim',
+    'numToStr/Comment.nvim',
+    version = nil,
+    cond = function()
+      return vim.fn.has 'nvim-0.10' == 0
+    end,
+    branch = 'master',
+    config = true,
+  },
+
+  { -- format PYTHON because it's ANNOYING
+    'Vimjas/vim-python-pep8-indent',
+  },
+  { -- generate docstrings
+    'danymat/neogen',
+    cmd = { 'Neogen' },
+    dependencies = 'nvim-treesitter/nvim-treesitter',
+    config = true,
+  },
+
+  {
+    'chrishrb/gx.nvim',
+    enabled = true,
+    keys = { { 'gx', '<cmd>Browse<cr>', mode = { 'n', 'x' } } },
+    cmd = { 'Browse' },
+    init = function()
+      vim.g.netrw_nogx = 1 -- disable netrw gx
+    end,
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    submodules = false, -- not needed, submodules are required only for tests
+    opts = {
+      handler_options = {
+        search_engine = 'duckduckgo',
+      },
+    },
+  },
+
+  -- interactive global search and replace
+  {
+    'nvim-pack/nvim-spectre',
+    cmd = { 'Spectre' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+  },
+}
