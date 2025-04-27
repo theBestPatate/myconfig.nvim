ec30f6fdb5087e6ff0255016c2558aff75a8f000
theBestPatate <jean-pierre.gninahophin@etu.univ-amu.fr>
2025-04-27 20:38:51 +0000

    more clean up

--- init.lua
+++ init.lua
@@ -904,20 +904,7 @@ require('lazy').setup({
   },
   { 'nvim-treesitter/nvim-treesitter-context', enabled = true },
 
-  -- require 'custom.plugins.editing',
-  -- require 'custom.plugins.dev',
-  -- require 'custom.plugins.note_taking',
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   { import = 'custom.plugins' },
-  --
-  -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
-  -- Or use telescope!
-  -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-  -- you can continue same window with `<space>sr` which resumes last telescope search
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
