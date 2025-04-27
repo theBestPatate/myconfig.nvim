41327877ee9f42aa831c51436eda7c52ee8b0b1d
theBestPatate <jean-pierre.gninahophin@etu.univ-amu.fr>
2025-04-27 21:47:30 +0000

    grouping the plugins and debloating the init file

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -1,6 +1,8 @@
 return {
   -- NOTE: All git commands directly from neovim
-  { 'tpope/vim-fugitive', enabled = true },
+  { 'tpope/vim-sleuth', enabled = true }, -- Detect tabstop and shiftwidth automatically
+  { 'tpope/vim-fugitive', enabled = true }, --GIT
+
   {
     -- NOTE: show indent lines
     'lukas-reineke/indent-blankline.nvim',
@@ -11,4 +13,56 @@ return {
     },
   },
   { 'jpalardy/vim-slime', enabled = true },
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+  { -- Autoformat
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_format = 'fallback' }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          return nil
+        else
+          return {
+            timeout_ms = 500,
+            lsp_format = 'fallback',
+          }
+        end
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- Conform can also run multiple formatters sequentially
+        python = { 'isort', 'black' },
+        --
+        -- You can use 'stop_after_first' to run the first available formatter from the list
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+    },
+  },
 }
