63d6fa03631a6a819635695161f044d72e7ea9ae
theBestPatate <jean-pierre.gninahophin@etu.univ-amu.fr>
2025-05-15 21:50:19 +0000

    tabout configuration

--- lua/plugins/ux.lua
+++ lua/plugins/ux.lua
@@ -161,12 +161,12 @@ return {
     lazy = false,
     config = function()
       require('tabout').setup {
-        tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
-        backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
+        tabkey = '<C-t>', -- key to trigger tabout, set to an empty string to disable
+        backwards_tabkey = '', -- key to trigger backwards tabout, set to an empty string to disable
         act_as_tab = true, -- shift content if tab out is not possible
         act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-        default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
-        default_shift_tab = '<C-d>', -- reverse shift default action,
+        default_tab = '<Tab>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+        default_shift_tab = '<S-Tab>', -- reverse shift default action,
         enable_backwards = true, -- well ...
         completion = false, -- if the tabkey is used in a completion pum
         tabouts = {
